7563fc3dee7567c99600294c9bcf0c70a42778d9
Sebastian Martinez <smariver@gmail.com>
2021-06-24 22:18:08 -0300

    Added responsive grid Added calculator interface plus made it responsive with 2 @media rules. Made font responsive with a css trick that uses px and vw.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function operate(operator, a, b){
-    
+    switch(operator){
+        case 0:
+            add(a,b);
+        case 1:
+            subtract(a,b);
+        case 2:
+            multiply(a,b);
+        case 3:
+            divide(a,b);
+    }
+}
+
+function add(a, b){
+    return a+b;
+}
+
+
+function subtract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
 }
