b2b3488b59fc00ecdb73c94186c4cfc719464038
Sebastian Martinez <smariver@gmail.com>
2021-06-27 21:19:21 -0300

    Fixed operate function and disabled buttons

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
-function operate(operator, a, b){
-    switch(operator){
-        case 0:
-            add(a,b);
-        case 1:
-            subtract(a,b);
-        case 2:
-            multiply(a,b);
-        case 3:
-            divide(a,b);
+function operate(op, a, b){
+    if(op === '+'){
+        return a + b;
+    }else if(op == '-'){
+        return a - b;
+    }else if(op == '*'){
+        return a * b;
+    }else if(op == '/'){
+        return a / b;
     }
 }
 
-function add(a, b){
-    return a+b;
-}
-
-
-function subtract(a, b){
-    return a-b;
-}
-
-function multiply(a, b){
-    return a*b;
-}
-
-function divide(a, b){
-    return a/b;
-}
 
 document.addEventListener('click', function(e){
     populateDisplay(e.target.id);
 });
 
 function populateDisplay(id){
-    let display = document.getElementById("numpad");
-    display.textContent += id;
-    let displayContent = display.textContent;
-    console.log(id);
-    if(!isNaN(parseInt(id))){
-        console.log("hello");
-        //document.getElementById(id).disabled = true;
+    let display = document.getElementById("numpad"); //gets the numpad element
+    
+    if(isNaN(parseInt(id))){ // if its not a number enters
+        display.textContent += " " + id + " ";
+        document.getElementById("x").disabled = true;
+        document.getElementById("/").disabled = true;
+        document.getElementById("+").disabled = true;
+        document.getElementById("-").disabled = true;
     }
+    else{   // else if it is a number entes and adds to display text
+        display.textContent += id;
+    }
+
+    let displayContent = display.textContent; // saves display value
+    console.log(displayContent);
 }
